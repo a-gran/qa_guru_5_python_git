97d140c2a528073a0c5cde5762a4f6fd34997fdd
Gran <awebdeff@gmail.com>
2025-11-04 17:38:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name} !!!')  # Press ⌘F8 to toggle the breakpoint.
     print('Bye!')
+    print('Conflict!')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
